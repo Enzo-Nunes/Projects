eb9703b355eaf48d707de00f3f55d89faa7db333
Enzo-Nunes <pedralhoenzo@gmail.com>
2023-01-03 01:15:52 +0000

    cfsaf

--- LP/Project.pl
+++ LP/Project.pl
@@ -2,4 +2,19 @@
 :- set_prolog_flag(answer_write_options,[max_depth(0)]). % para listas completas
 :- ['dados.pl'], ['keywords.pl']. % ficheiros a importar.
 
-eventosSemSalas(EventosSemSalas) :- findall(ID, evento(ID, _, _, _, semSala), EventosSemSalas). % 
+eventosSemSalas(EventosSemSala) :-
+    findall(E, evento(E,_,_,_,semSala), EventosSemSala).
+
+eventosSemSalasDiaSemana(DiaDaSemana, EventosSemSala) :-
+    findall(E, (evento(E,_,_,_,semSala), horario(E,DiaDaSemana,_,_,_,_)), EventosSemSala).
+
+eventosSemSalasPeriodo(ListaPeriodos, EventosSemSala) :-
+    adicionaSemestres(ListaPeriodos, ListaNova),
+    findall(E, (evento(E,_,_,_,semSala), horario(E,_,_,_,_,P), member(P, ListaNova)), EventosSemSala).
+
+% Predicado auxiliar para adicionar os semestres 1_2 e 3_4 à ListaPeriodos.
+adicionaSemestres(ListaPeriodos, ListaNova) :-
+    member(p1, ListaPeriodos),                                 ListaNova = [p1_2|ListaPeriodos], !;
+    member(p2, ListaPeriodos), not(member(p1, ListaPeriodos)), ListaNova = [p1_2|ListaPeriodos], !;
+    member(p3, ListaPeriodos),                                 ListaNova = [p3_4|ListaPeriodos], !;
+    member(p4, ListaPeriodos), not(member(p3, ListaPeriodos)), ListaNova = [p3_4|ListaPeriodos], !.
