8df7d9b01bdbb8033b7fd0447e5c51ff078a86b9
Enzo-Nunes <pedralhoenzo@gmail.com>
2023-01-09 19:51:25 +0000

    last exercide first attempt

--- LP/test2.pl
+++
@@ -1,4 +0,0 @@
-Resultados = [casosCriticos(segunda-feira,grandesAnfiteatros,89),
-    casosCriticos(segunda-feira,grandesAnfiteatros,95),
-    casosCriticos(segunda-feira,pequenosAnfiteatros,93),
-    casosCriticos(sexta-feira,labsQuimica,89)].
