b4385fbb56f98ed7ad0d7525789f6e5f2c92ecc1
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2023-03-13 20:23:49 +0000

    command c done

--- IAED/project1.c
+++ IAED/project1.c
@@ -12,27 +12,26 @@
 #define MAX_LINES 200
 
 
-stop stop_list[MAX_STOPS];
-int nr_stops = 0;
-
-line line_list[MAX_LINES];
-int nr_lines = 0;
-
-
-typedef struct {
-    char line_name[LINE_NAME_SIZE];
-    /*char origin[STOP_NAME_SIZE];
-    char destination[STOP_NAME_SIZE];*/
-    char course[MAX_STOPS];
-    int nr_line_stops;
-    float total_cost, total_duration;
-} line;
-
 typedef struct {
     char stop_name[STOP_NAME_SIZE];
     float lat;
     float lon;
 } stop;
+
+typedef struct {
+    char line_name[LINE_NAME_SIZE];
+    stop course[MAX_STOPS];
+    int nr_line_stops;
+    float total_cost, total_duration;
+} line;
+
+
+line line_list[MAX_LINES];
+stop stop_list[MAX_STOPS];
+
+int nr_lines  = 0;
+int nr_stops  = 0;
+int nr_intsec = 0;
 
 
 char* readNextWord(char buffer[]) {
@@ -44,13 +43,13 @@ char* readNextWord(char buffer[]) {
         index++;
     }
 
-    for (;
+    while (
         buffer[index] != ' ' &&
         buffer[index] != '\n' &&
-        buffer[index] != '\0';
-        i++, index++
-        ) {
+        buffer[index] != '\0'
+    ) {
         next_word[i] = buffer[index];
+        i++, index++;
     }
 
     next_word[i] = '\0';
@@ -59,43 +58,31 @@ char* readNextWord(char buffer[]) {
 }
 
 
-void lineCommand(char buffer[]) {
-
-    char *l;
-
-    if (readNextWord(buffer) == NULL) {
-        listLines();
-    } else {
-        l = readNextWord(buffer);
-        if (isLine (l) == 1) {
-            listLineStops(l);
-        } else {
-            createLine(l, buffer);
-        }
-    }
-}
-
 int isLine(char l[]) {
     int i;
 
     for (i = 0; i < nr_lines; i++) {
         if (strcmp(l, line_list[i].line_name) == 0) {
-            return 1;
+            return i;
         }
     }
 
-    return 0;
+    return -1;
 }
 
 void listLines() {
     int i;
 
     for (i = 0; i < nr_lines; i++) {
-        printf(
-            "%s %s %s %d %.2f %.2f\n",
-                line_list[i].line_name,
-                line_list[i].course[0],
-                line_list[i].course[line_list[i].nr_line_stops - 1],
+        printf("%s ", line_list[i].line_name);
+
+        if (line_list[i].nr_line_stops > 0) {
+            printf("%s %s ",
+                line_list[i].course[0].stop_name,
+                line_list[i].course[line_list[i].nr_line_stops - 1].stop_name);
+        }
+
+        printf( "%d %.2f %.2f\n",
                 line_list[i].nr_line_stops,
                 line_list[i].total_cost,
                 line_list[i].total_duration
@@ -103,30 +90,78 @@ void listLines() {
     }
 }
 
-void listLineStops(char l[]) {
-    
+void listLineStops(int i, char buffer[]) {
+    int j, reverse = 0;
+    line current_line;
+    char *flag;
+
+    current_line = line_list[i];
+
+    /*Check if the command is reversed*/
+    flag = readNextWord(buffer);
+    if (flag != NULL) {
+        if (strncmp(flag, "inverso", 3) == 0) {
+            reverse = 1;
+        } else {
+            printf("incorrect sort option.\n");
+            return;
+        }
+    }
+
+    /*Print the stops*/
+    if (reverse == 0) {
+        for (j = 0; j < current_line.nr_line_stops; j++) {
+            printf("%s\n", current_line.course[j].stop_name);
+        }
+    } else {
+        for (j = current_line.nr_line_stops - 1; j >= 0; j--) {
+            printf("%s\n", current_line.course[j].stop_name);
+        }
+    }
 }
 
-void createLine(char l, char buffer[]) {
-    line l;
+void createLine(char *l) {
+    line new_line;
+
+    strcpy(new_line.line_name, l);
+    new_line.nr_line_stops = 0;
+    new_line.total_cost = 0;
+    new_line.total_duration = 0;
+
+    line_list[nr_lines] = new_line;
+    nr_lines++;
 }
 
+void lineCommand(char buffer[]) {
+
+    char *l;
+    int i;
+
+    if (readNextWord(buffer) == NULL) {
+        listLines();
+    } else {
+        l = readNextWord(buffer);
+        if ((i = isLine (l)) != -1) {
+            listLineStops(i, buffer);
+        } else {
+            createLine(l);
+        }
+    }
+}
 
 
 int main() {
 
     char buffer[BUFFER_SIZE], c;
-    int count = 0;
+    int count;
 
     while (1) {
 
         memset(buffer, 0, BUFFER_SIZE);
 
-        while ((c = getchar()) != '\n') {
+        for (count = 0; (c = getchar()) != '\n'; count++) {
             buffer[count] = c;
-            count++;
         }
-        buffer[count] = '\0';
 
         switch (buffer[0]) {
             case 'q':
@@ -134,17 +169,6 @@ int main() {
             case 'c':
                 lineCommand(buffer);
                 break;
-            case 'p':
-                stopCommand(buffer);
-                break;
-            case 'l':
-                linkCommand(buffer);
-                break;
-            case 'i':
-                intsecCommand(buffer);
-                break;
         }
     }
-
-    return 0;
 }
