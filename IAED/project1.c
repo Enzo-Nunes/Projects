ec28db4d22721f6461f39269022f6c057d8e1e62
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2023-03-16 02:55:03 +0000

    project complete

--- IAED/project1.c
+++ IAED/project1.c
@@ -20,7 +20,7 @@ typedef struct {
 typedef struct {
     char line_name[LINE_NAME_SIZE];
     stop course[MAX_STOPS];
-    int nr_line_stops;
+    int nr_line_stops, is_cycle;
     double total_cost, total_duration;
 } line;
 
@@ -36,7 +36,6 @@ int buffer_index = 0;
 /* Global variables to keep track of the numbers of lines and stops. */
 int nr_lines = 0;
 int nr_stops = 0;
-int nr_intsec = 0;
 
 /* Function that reads the next word in the buffer and returns it. */
 char *readNextWord(char buffer[]) {
@@ -65,6 +64,31 @@ char *readNextWord(char buffer[]) {
     next_word[i] = '\0';
 
     return (i == 0) ? NULL : next_word;
+}
+
+line *sortLines(line lines_list[]) {
+
+    int i, j, min_index;
+
+    line *sorted_lines_list = malloc(nr_lines * sizeof(line));
+    memcpy(sorted_lines_list, lines_list, nr_lines * sizeof(line));
+
+    for (i = 0; i < nr_lines - 1; i++) {
+        min_index = i;
+        for (j = i + 1; j < nr_lines; j++) {
+            if (strcmp(sorted_lines_list[j].line_name,
+                       sorted_lines_list[min_index].line_name) < 0) {
+                min_index = j;
+            }
+        }
+        if (min_index != i) {
+            line temp = sorted_lines_list[i];
+            sorted_lines_list[i] = sorted_lines_list[min_index];
+            sorted_lines_list[min_index] = temp;
+        }
+    }
+
+    return sorted_lines_list;
 }
 
 int isLine(char l[]) {
@@ -116,14 +140,15 @@ void listLineStops(int i, char buffer[]) {
 
     /*Print the stops*/
     if (reverse == 0) {
-        for (j = 0; j < current_line.nr_line_stops; j++) {
-            printf("%s\n", current_line.course[j].stop_name);
+        for (j = 0; j < current_line.nr_line_stops - 1; j++) {
+            printf("%s, ", current_line.course[j].stop_name);
         }
     } else {
-        for (j = current_line.nr_line_stops - 1; j >= 0; j--) {
-            printf("%s\n", current_line.course[j].stop_name);
+        for (j = current_line.nr_line_stops - 1; j > 0; j--) {
+            printf("%s, ", current_line.course[j].stop_name);
         }
     }
+    printf("%s\n", current_line.course[j].stop_name);
 }
 
 void createLine(char *l) {
@@ -133,6 +158,7 @@ void createLine(char *l) {
     new_line.nr_line_stops = 0;
     new_line.total_cost = 0;
     new_line.total_duration = 0;
+    new_line.is_cycle = 0;
 
     line_list[nr_lines] = new_line;
     nr_lines++;
@@ -179,6 +205,7 @@ int nrStopLines(char s[]) {
         for (j = 0; j < line_list[i].nr_line_stops; j++) {
             if (strcmp(s, line_list[i].course[j].stop_name) == 0) {
                 count++;
+                break;
             }
         }
     }
@@ -236,21 +263,162 @@ void stopCommand(char buffer[]) {
     }
 }
 
+int isLinkLineCompatible(int line_index, int origin_index,
+                         int destination_index) {
+
+    if (line_list[line_index].is_cycle == 1) {
+        printf("link cannot be associated with bus line.\n");
+        return -1;
+    }
+
+    if (strcmp(line_list[line_index]
+                   .course[line_list[line_index].nr_line_stops - 1]
+                   .stop_name,
+               stop_list[origin_index].stop_name) == 0) {
+        return 1;
+    }
+
+    if (strcmp(line_list[line_index].course[0].stop_name,
+
+               stop_list[destination_index].stop_name) == 0) {
+        return 0;
+    }
+
+    printf("link cannot be associated with bus line.\n");
+    return -1;
+}
+
+int isValidLink(char *line_pre, char *origin_pre, char *destination_pre,
+                double cost_pre, double duration_pre) {
+
+    int line_index, origin_index, destination_index;
+
+    line_index = isLine(line_pre);
+    origin_index = isStop(origin_pre);
+    destination_index = isStop(destination_pre);
+
+    if (line_index == -1) {
+        printf("%s: no such line.\n", line_pre);
+        return -1;
+    }
+
+    if (origin_index == -1) {
+        printf("%s: no such stop.\n", origin_pre);
+        return -1;
+    }
+
+    if (destination_index == -1) {
+        printf("%s: no such stop.\n", destination_pre);
+        return -1;
+    }
+
+    if ((cost_pre < 0) || (duration_pre < 0)) {
+        printf("negative cost or duration.\n");
+        return -1;
+    }
+
+    if (line_list[line_index].nr_line_stops == 0) {
+        return 2;
+    }
+
+    return isLinkLineCompatible(line_index, origin_index, destination_index);
+}
+
+void createFirstStops(int line_index, int origin_index, int destination_index,
+                      double cost_pre, double duration_pre) {
+
+    line_list[line_index].course[0] = stop_list[origin_index];
+    line_list[line_index].course[1] = stop_list[destination_index];
+    line_list[line_index].nr_line_stops = 2;
+    line_list[line_index].total_cost = cost_pre;
+    line_list[line_index].total_duration = duration_pre;
+}
+
+void createLink(int line_index, int origin_index, int destination_index,
+                double cost_pre, double duration_pre, int link_info) {
+
+    int i;
+    switch (link_info) {
+    case 0: {
+        for (i = line_list[line_index].nr_line_stops - 1; i >= 0; i--) {
+            line_list[line_index].course[i + 1] =
+                line_list[line_index].course[i];
+        }
+        line_list[line_index].course[0] = stop_list[origin_index];
+    } break;
+
+    case 1: {
+        line_list[line_index].course[line_list[line_index].nr_line_stops] =
+            stop_list[destination_index];
+    } break;
+
+    case 2: {
+        createFirstStops(line_index, origin_index, destination_index, cost_pre,
+                         duration_pre);
+    }
+        return;
+    }
+
+    line_list[line_index].nr_line_stops++;
+    line_list[line_index].total_cost += cost_pre;
+    line_list[line_index].total_duration += duration_pre;
+
+    if (line_list[line_index].course[0].stop_name ==
+        line_list[line_index]
+            .course[line_list[line_index].nr_line_stops - 1]
+            .stop_name) {
+        line_list[line_index].is_cycle = 1;
+    }
+}
+
 void linkCommand(char buffer[]) {
 
-    char *l, *s;
+    int line_index, origin_index, destination_index;
+    double cost_pre, duration_pre;
 
-    l = readNextWord(buffer);
-    if (isLine(l) == -1) {
-        printf("%s: no such line.\n", l);
-        return;
-    } else {
-        s = readNextWord(buffer);
-        if (isStop(s) == -1) {
-            printf("%s: no such stop.\n", s);
-            return;
-        } else {
-            if () {
+    /* -1 if not possible, 0 if origin link, 1 if destination or cycle link, 2
+     * if first stops.*/
+    int link_info;
+    char *line_pre, *origin_pre, *destination_pre;
+
+    line_pre = readNextWord(buffer);
+    origin_pre = readNextWord(buffer);
+    destination_pre = readNextWord(buffer);
+
+    line_index = isLine(line_pre);
+    origin_index = isStop(origin_pre);
+    destination_index = isStop(destination_pre);
+    cost_pre = atof(readNextWord(buffer));
+    duration_pre = atof(readNextWord(buffer));
+
+    if ((link_info = isValidLink(line_pre, origin_pre, destination_pre,
+                                 cost_pre, duration_pre)) != -1) {
+        createLink(line_index, origin_index, destination_index, cost_pre,
+                   duration_pre, link_info);
+    }
+    return;
+}
+
+void intsecCommand() {
+
+    int i, j, k;
+    char *stop_pre;
+    line *sorted_lines_list = sortLines(line_list);
+
+    for (i = 0; i < nr_stops; i++) {
+        stop_pre = stop_list[i].stop_name;
+        if (nrStopLines(stop_pre) > 1) {
+            printf("%s %d:", stop_pre, nrStopLines(stop_pre));
+            for (j = 0; j < nr_lines; j++) {
+                for (k = 0; k < sorted_lines_list[j].nr_line_stops; k++) {
+                    if (strcmp(stop_pre,
+                               sorted_lines_list[j].course[k].stop_name) == 0) {
+                        printf(" %s", sorted_lines_list[j].line_name);
+                        break;
+                    }
+                }
+            }
+            printf("\n");
         }
     }
 }
@@ -280,6 +448,9 @@ int main() {
             break;
         case 'l':
             linkCommand(buffer + 2);
+            break;
+        case 'i':
+            intsecCommand();
             break;
         }
     }
