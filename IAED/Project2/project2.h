87f8e4e1d4b81a7ecd85ae1b9d33ed323c911173
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2023-04-01 23:38:50 +0100

    constants and comments

--- IAED/Project2/project2.h
+++ IAED/Project2/project2.h
@@ -7,13 +7,31 @@
 
 #define BUFFER_SIZE 65536
 #define REVERSE_FLAG "inverso"
+#define REVERSE_FLAG_SIZE 8
 #define CHUNK_SIZE 64
 
+#define TRUE 1
+#define FALSE 0
+#define NOT_FOUND -1
+
+/* Link types. */
+#define INVALID_LINK -1
+#define ORIGIN_LINK 0
+#define DESTINATION_LINK 1
+#define FIRST_STOPS 2
+
+/*
+ * Stop structure. Defined by name and coordinates only.
+ */
 typedef struct Stop {
     char *name;
     double lat, lon;
 } Stop;
 
+/*
+ * StopNode structure. Defined by a pointer to a Stop, the cost and duration
+ * to the next stop, and pointers to the next and previous nodes.
+ */
 typedef struct StopNode {
     Stop *stop;
     double cost_next, duration_next;
@@ -21,6 +39,11 @@ typedef struct StopNode {
     struct StopNode *prev;
 } StopNode;
 
+/*
+ * Line structure. Defined by name, pointers to origin and destination stop
+ * nodes, the number of stops in the line, and the total cost and duration of
+ * the line.
+ */
 typedef struct Line {
     char *name;
     StopNode *origin;
@@ -29,6 +52,11 @@ typedef struct Line {
     double cost, duration;
 } Line;
 
+/*
+ * BusNetwork system structure. Defined by the number of lines and stops, a
+ * buffer index for the readNextWord function, and pointers to the line and stop
+ * lists.
+ */
 typedef struct BusNetwork {
     int nr_lines, nr_stops, buffer_index;
     Line *line_list;
