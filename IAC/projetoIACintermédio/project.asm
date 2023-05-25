beb1345d0976c4d1105031d28fb93a584967495c
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2023-05-25 17:35:42 +0100

    final touches

--- IAC/projetoIACintermédio/project.asm
+++ IAC/projetoIACintermédio/project.asm
@@ -26,11 +26,11 @@ MEXE_SONDA					EQU 0FH
 
 ; MediaCenter
 
-APAGA_ECRA	 		     	EQU 6002H			; endereço do comando para apagar todos os pixels já desenhados
-DEFINE_LINHA    		 	EQU 600AH			; endereço do comando para definir a linha
-DEFINE_COLUNA   	 	 	EQU 600CH			; endereço do comando para definir a coluna
-DEFINE_PIXEL    	 	 	EQU 6012H			; endereço do comando para escrever um pixel
-APAGA_AVISO     		 	EQU 6040H			; endereço do comando para apagar o aviso de nenhum cenário selecionado
+APAGA_ECRA	 				EQU 6002H			; endereço do comando para apagar todos os pixels já desenhados
+DEFINE_LINHA				EQU 600AH			; endereço do comando para definir a linha
+DEFINE_COLUNA				EQU 600CH			; endereço do comando para definir a coluna
+DEFINE_PIXEL				EQU 6012H			; endereço do comando para escrever um pixel
+APAGA_AVISO					EQU 6040H			; endereço do comando para apagar o aviso de nenhum cenário selecionado
 SELECIONA_CENARIO_FUNDO		EQU 6042H			; endereço do comando para selecionar uma imagem de fundo
 	CENARIO_JOGO			EQU 0				; cenário de jogo
 TOCA_SOM					EQU 605AH			; endereço do comando para tocar um som
@@ -38,20 +38,20 @@ TOCA_SOM					EQU 605AH			; endereço do comando para tocar um som
 
 ; Dimensões Objetos
 
-ALTURA_ASTEROIDE			EQU 5				;altura do objeto asteroide
-LARGURA_ASTEROIDE			EQU 5				;largura do objeto asteroide
-LIN_ASTEROIDE_INIC			EQU 0				;linha referencia do asteroide
-COL_ASTEROIDE_INIC			EQU 0				;coluna referencia do asteroide
+ALTURA_ASTEROIDE			EQU 5				; altura do objeto asteroide
+LARGURA_ASTEROIDE			EQU 5				; largura do objeto asteroide
+LIN_ASTEROIDE_INIC			EQU 0				; linha referencia do asteroide
+COL_ASTEROIDE_INIC			EQU 0				; coluna referencia do asteroide
 
-ALTURA_SONDA				EQU 1				;altura do objeto sonda
-LARGURA_SONDA				EQU 1				;largura do objeto sonda
-LIN_SONDA_INIC				EQU 26				;linha referencia da sonda
-COL_SONDA_INIC				EQU 32				;coluna referencia da sonda	
+ALTURA_SONDA				EQU 1				; altura do objeto sonda
+LARGURA_SONDA				EQU 1				; largura do objeto sonda
+LIN_SONDA_INIC				EQU 26				; linha referencia da sonda
+COL_SONDA_INIC				EQU 32				; coluna referencia da sonda	
 
-ALTURA_PAINEL				EQU 5				;altura do objeto painel
-LARGURA_PAINEL				EQU 15				;largura do objeto painel
-LIN_PAINEL_INIC				EQU 27				;linha referencia do painel
-COL_PAINEL_INIC				EQU 25				;linha referencia do painel
+ALTURA_PAINEL				EQU 5				; altura do objeto painel
+LARGURA_PAINEL				EQU 15				; largura do objeto painel
+LIN_PAINEL_INIC				EQU 27				; linha referencia do painel
+COL_PAINEL_INIC				EQU 25				; linha referencia do painel
 
 ; Cores
 
@@ -136,7 +136,7 @@ CALL desenha_painel								; desenha o painel da nave
 
 ciclo_teclado:									; ciclo que vai repetidamente esperar uma ação no teclado
 	CALL inicio_teclado
-	JMP verifica_tecla
+	JMP  verifica_tecla
 
 fim:
 	JMP fim
@@ -190,14 +190,14 @@ converte_num:									; converte a tecla premida para um número hexadecimal ent
 	SHL  R8, 2									; multiplica o número correspondente à linha por quatro
 	MOV  R1, R0         
 	CALL converte_num_ciclo						; converte a coluna
-	POP R9
-	POP R6
-	POP R5
-	POP R4
-	POP R3
-	POP R2
-	POP R1
-	POP R0
+	POP  R9
+	POP  R6
+	POP  R5
+	POP  R4
+	POP  R3
+	POP  R2
+	POP  R1
+	POP  R0
 	RET
 converte_num_ciclo:								; acumula o número de SHR feitos até o valor da linha/coluna ser 0    
 	SHR  R1, 1									
@@ -229,7 +229,7 @@ verifica_tecla:									; determina qual deve ser a ação consoante a tecla pre
 	JMP	ha_tecla								; aguarda que a tecla deixe de ser premida
 
 ; ******************************************************************************
-; * ha_tecla - 			Função que espera que o utilizador deixe de premir a
+; * ha_tecla -			Função que espera que o utilizador deixe de premir a
 ; * 					tecla.
 ; ******************************************************************************
 
@@ -241,14 +241,14 @@ ha_tecla:										; neste ciclo espera-se até NENHUMA tecla estar premida
 	PUSH R4
 	ciclo_ha_tecla:
 		MOV  R1, R7								; testar a última linha usada (R1 tinha sido alterado)
-		MOV R2, TEC_LIN
-		MOV R3, TEC_COL
-		MOV R4, MASCARA
+		MOV  R2, TEC_LIN
+		MOV  R3, TEC_COL
+		MOV  R4, MASCARA
 		MOVB [R2], R1							; escrever no periférico de saída (linhas)
 		MOVB R0, [R3]							; ler do periférico de entrada (colunas)
 		AND  R0, R4								; elimina bits para além dos bits 0-3
-		CMP  R0, 0 								; há tecla premida?
-		JNZ  ciclo_ha_tecla 					; se ainda houver uma tecla premida, espera até não haver
+		CMP  R0, 0								; há tecla premida?
+		JNZ  ciclo_ha_tecla						; se ainda houver uma tecla premida, espera até não haver
 	POP R4
 	POP R3
 	POP R2
@@ -261,17 +261,17 @@ ha_tecla:										; neste ciclo espera-se até NENHUMA tecla estar premida
 ; ******************************************************************************
 
 ; ******************************************************************************
-; * incrementa_display- incrementa o valor dos displays por 1
+; * incrementa_display - incrementa o valor dos displays por 1
 ; ******************************************************************************
 
 incrementa_display:
 	PUSH R9
-	MOV R9, [VALOR_DISPLAYS]					; valor atual dos displays
-	ADD R9, 1									; incrementa o valor em 1
-	MOV [DISPLAYS], R9							; atualiza os displays
-	MOV [VALOR_DISPLAYS], R9					; atualiza a variável do valor dos displays
-	POP R9
-	JMP ha_tecla								; aguarda que a tecla deixe de ser premida
+	MOV  R9, [VALOR_DISPLAYS]					; valor atual dos displays
+	ADD  R9, 1									; incrementa o valor em 1
+	MOV  [DISPLAYS], R9							; atualiza os displays
+	MOV  [VALOR_DISPLAYS], R9					; atualiza a variável do valor dos displays
+	POP  R9
+	JMP  ha_tecla								; aguarda que a tecla deixe de ser premida
 	
 ; ******************************************************************************
 ; * decrementa_display - decrementa o valor dos displays por 1
@@ -279,39 +279,39 @@ incrementa_display:
 
 decrementa_display:
 	PUSH R9
-	MOV R9, [VALOR_DISPLAYS]					; valor atual dos displays
-	SUB R9, 1									; decrementa o valor em 1
-	MOV [DISPLAYS], R9							; atualiza os displays
-	MOV [VALOR_DISPLAYS], R9					; atualiza a variável do valor dos displays
-	POP R9
-	JMP ha_tecla								; aguarda que a tecla deixe de ser premida
+	MOV  R9, [VALOR_DISPLAYS]					; valor atual dos displays
+	SUB  R9, 1									; decrementa o valor em 1
+	MOV  [DISPLAYS], R9							; atualiza os displays
+	MOV  [VALOR_DISPLAYS], R9					; atualiza a variável do valor dos displays
+	POP  R9
+	JMP  ha_tecla								; aguarda que a tecla deixe de ser premida
 
 ; ******************************************************************************
-; * mexer_asteroide - 	avança o asteroide para a próxima posição
+; * mexer_asteroide -	avança o asteroide para a próxima posição
 ; ******************************************************************************
 
 mexer_asteroide:
 	PUSH R1
 	PUSH R2
 	PUSH R3
-	MOV R1, [REF_ASTEROIDE]						; linha do pixel de referência do asteroide
-	MOV R2, [REF_ASTEROIDE + 2]					; coluna do pixel de referência do asteroide
-	MOV R3, DEF_ASTEROIDE						; tabela do desenho do asteroide
+	MOV  R1, [REF_ASTEROIDE]					; linha do pixel de referência do asteroide
+	MOV  R2, [REF_ASTEROIDE + 2]				; coluna do pixel de referência do asteroide
+	MOV  R3, DEF_ASTEROIDE						; tabela do desenho do asteroide
 	CALL apaga_objeto							; apaga o asteroide
-	ADD R1, 1									; incrementa em 1 a linha
-	ADD R2, 1									; incrementa em 1 a coluna
-	MOV [REF_ASTEROIDE], R1						; atualiza a linha do pixel de referência
-	MOV [REF_ASTEROIDE + 2], R2					; atualiza a coluna do pixel de referência
+	ADD  R1, 1									; incrementa em 1 a linha
+	ADD  R2, 1									; incrementa em 1 a coluna
+	MOV  [REF_ASTEROIDE], R1					; atualiza a linha do pixel de referência
+	MOV  [REF_ASTEROIDE + 2], R2				; atualiza a coluna do pixel de referência
 	CALL desenha_asteroide						; desenha novamente o asteroide
-	MOV R1, SFX_AK_47							; seleciona o efeito sonoro
-	MOV [TOCA_SOM], R1							; reproduz o efeito sonoro
-	POP R3
-	POP R2
-	POP R1
-	JMP ha_tecla								; aguarda que a tecla deixe de ser premida
+	MOV  R1, SFX_AK_47							; seleciona o efeito sonoro
+	MOV  [TOCA_SOM], R1							; reproduz o efeito sonoro
+	POP  R3
+	POP  R2
+	POP  R1
+	JMP  ha_tecla								; aguarda que a tecla deixe de ser premida
 	
 ; ******************************************************************************
-; * mexer_sonda - 		apaga a sonda, decrementa por 1 a lin do pixel de
+; * mexer_sonda -		apaga a sonda, decrementa por 1 a lin do pixel de
 ; *						referencia e desenha-a outra vez
 ; ******************************************************************************
 
@@ -319,39 +319,39 @@ mexer_sonda:
 	PUSH R1
 	PUSH R2
 	PUSH R3
-	MOV R1, [REF_SONDA]							; linha do pixel de referência da sonda
-	MOV R2, [REF_SONDA + 2]						; coluna do pixel de referência da sonda
-	MOV R3, DEF_SONDA							; tabela do desenho da sonda
+	MOV  R1, [REF_SONDA]						; linha do pixel de referência da sonda
+	MOV  R2, [REF_SONDA + 2]					; coluna do pixel de referência da sonda
+	MOV  R3, DEF_SONDA							; tabela do desenho da sonda
 	CALL apaga_objeto							; apaga a sonda
-	SUB R1, 1									; decrementa em 1 a linha
-	MOV [REF_SONDA], R1							; atualiza a linha do pixel de referência
+	SUB  R1, 1									; decrementa em 1 a linha
+	MOV  [REF_SONDA], R1						; atualiza a linha do pixel de referência
 	CALL desenha_sonda							; desenha novamente a sonda
-	POP R3
-	POP R2
-	POP R1
-	JMP ha_tecla								; aguarda que a tecla deixe de ser premida
+	POP  R3
+	POP  R2
+	POP  R1
+	JMP  ha_tecla								; aguarda que a tecla deixe de ser premida
 
 
 ; ******************************************************************************
-; * desenha_asteroide - chama a funcao desenha_objeto com os argumentos
-; *                 	para desenhar o asteroide.
+; * desenha_asteroide -	chama a funcao desenha_objeto com os argumentos
+; *						para desenhar o asteroide.
 ; ******************************************************************************
 
 desenha_asteroide:
 	PUSH R1
 	PUSH R2
 	PUSH R3
-	MOV R1, [REF_ASTEROIDE]
-	MOV R2, [REF_ASTEROIDE + 2]
-	MOV R3, DEF_ASTEROIDE
+	MOV  R1, [REF_ASTEROIDE]
+	MOV  R2, [REF_ASTEROIDE + 2]
+	MOV  R3, DEF_ASTEROIDE
 	CALL desenha_objeto
-	POP R3
-	POP R2
-	POP R1
+	POP  R3
+	POP  R2
+	POP  R1
 	RET
 	
 ; ******************************************************************************
-; * desenha_sonda - 	chama a funcao desenha_objeto com os argumentos
+; * desenha_sonda -		chama a funcao desenha_objeto com os argumentos
 ; *						necessarios para desenhar a sonda.
 ; ******************************************************************************
 
@@ -359,36 +359,36 @@ desenha_sonda:
 	PUSH R1
 	PUSH R2
 	PUSH R3
-	MOV R1, [REF_SONDA]
-	MOV R2, [REF_SONDA + 2]
-	MOV R3, DEF_SONDA
+	MOV  R1, [REF_SONDA]
+	MOV  R2, [REF_SONDA + 2]
+	MOV  R3, DEF_SONDA
 	CALL desenha_objeto
-	POP R3
-	POP R2
-	POP R1
+	POP  R3
+	POP  R2
+	POP  R1
 	RET
 	
 ; ******************************************************************************
-; * desenha_painel - 	chama a funcao desenha_objeto com os argumentos
-; *                 	necessarios para desenhar o painel da nave.
+; * desenha_painel -	chama a funcao desenha_objeto com os argumentos
+; * 					necessarios para desenhar o painel da nave.
 ; ******************************************************************************
 
 desenha_painel:
 	PUSH R1
 	PUSH R2
 	PUSH R3
-	MOV R1, [REF_PAINEL]
-	MOV R2, [REF_PAINEL + 2]
-	MOV R3, DEF_PAINEL
+	MOV  R1, [REF_PAINEL]
+	MOV  R2, [REF_PAINEL + 2]
+	MOV  R3, DEF_PAINEL
 	CALL desenha_objeto
-	POP R3
-	POP R2
-	POP R1
+	POP  R3
+	POP  R2
+	POP  R1
 	RET
 
 
 ; ******************************************************************************
-; * desenha_objeto - 	desenha um objeto respeitando os argumentos:
+; * desenha_objeto -	desenha um objeto respeitando os argumentos:
 ; *						R1 - linha do pixel de referência
 ; *						R2 - coluna do pixel de referência
 ; *						R3 - tabela do objeto
@@ -415,7 +415,7 @@ desenha_objeto:									; desenha o objeto a partir da tabela
 		MOV [DEFINE_COLUNA], R2					; seleciona a coluna
 		MOV [DEFINE_PIXEL], R4					; altera a cor do pixel na linha e coluna selecionadas
 		ADD	R3, 2								; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
-		ADD  R2, 1              				; próxima coluna
+		ADD  R2, 1								; próxima coluna
 		SUB R7, 1								; menos uma coluna para tratar
 		JNZ desenha_linha						; continua até percorrer toda a largura do objeto
 		MOV R2, R8								; volta á primeira linha
@@ -434,7 +434,7 @@ desenha_objeto:									; desenha o objeto a partir da tabela
 	RET
 	
 ; ******************************************************************************
-; *	apaga_objeto - 		apaga um objeto respeitando os argumentos:
+; *	apaga_objeto -		apaga um objeto respeitando os argumentos:
 ; *						R1 - linha do pixel de referência
 ; *						R2 - coluna do pixel de referência
 ; *						R3 - tabela do objeto
@@ -455,11 +455,11 @@ apaga_objeto:									; apaga o objeto a partir da tabela
 	MOV  R6, [R3]								; obtém o largura do objeto
 	MOV  R7, R6									; decrementador da coluna
 	MOV  R3, 0									; valor do pixel transparente ( irá substituir todos os outros)
-	apaga_linha:       							; desenha os pixels do objeto a partir da tabela
+	apaga_linha:								; desenha os pixels do objeto a partir da tabela
 		MOV [DEFINE_LINHA], R1					; seleciona a linha
 		MOV [DEFINE_COLUNA], R2					; seleciona a coluna
 		MOV [DEFINE_PIXEL], R3					; altera a cor do pixel na linha e coluna selecionadas
-		ADD R2, 1              					; próxima coluna
+		ADD R2, 1								; próxima coluna
 		SUB R7, 1								; menos uma coluna para tratar
 		JNZ apaga_linha							; continua até percorrer toda a largura do objeto
 		MOV R2, R8								; volta á primeira coluna
