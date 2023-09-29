34ebc64828041ab639169fa368c61a3606647fbb
ArchKaiser <pedralhoenzo@gmail.com>
2023-09-29 16:27:21 +0100

    skeleton + racism

---
+++ PO/xxl/UMLracisms/Spreadsheet.java
@@ -0,0 +1,22 @@
+package xxl.UMLracisms;
+
+public class Spreadsheet {
+	User[]		_owners;
+	Cell[]		_cells;
+	CutBuffer	_cutBuffer;
+
+	public Spreadsheet(User owner, int width, int height) {
+		_owners = owner;
+		// FIXME implement cell creation
+	}
+
+	public void addOwner(User owner) {
+		// FIXME implement method
+	}
+	
+	public void removeOwner(User owner) {
+		// FIXME implement method
+	}
+
+	public void 
+}
