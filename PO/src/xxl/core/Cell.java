9cfb2c6dd010fc6594d88d356457250f3822753f
Didas <ist1106196@tecnico.ulisboa.pt>
2023-10-12 01:12:52 +0100

    Removed dependencies system

--- PO/src/xxl/core/Cell.java
+++ PO/src/xxl/core/Cell.java
@@ -4,23 +4,17 @@ import xxl.core.exception.IncorrectValueTypeException;
 
 public class Cell {
 	Position _position;
-	Cell[] _dependants;
 	CellValue _content;
-
-	public Cell(int posX, int posY) {
-		_position = new Position(posX, posY);
-	}
 
 	public Cell(Position pos){
 		_position = pos;
 	}
-
-	public void update(CellValue value) {
+	public void update(CellValue value) throws IncorrectValueTypeException {
 		_content = value;
-		//TODO: Update dependants
 	}
 
 	public ValueWrapper getValue() throws IncorrectValueTypeException {
+		_content.recalculate();
 		return _content.getValue();
 	}
 }
