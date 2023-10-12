6fa00a386b15a33b08534e980e54dfdcbbd33fea
ArchKaiser <pedralhoenzo@gmail.com>
2023-10-12 01:24:09 +0100

    commands done

--- PO/src/xxl/app/main/DoOpen.java
+++ PO/src/xxl/app/main/DoOpen.java
@@ -1,8 +1,9 @@
 package xxl.app.main;
 
-import pt.tecnico.uilib.forms.Form;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import xxl.app.exception.FileOpenFailedException;
+import xxl.core.exception.UnavailableFileException;
 import xxl.core.Calculator;
 
 /**
@@ -12,13 +13,13 @@ class DoOpen extends Command<Calculator> {
 
 	DoOpen(Calculator receiver) {
 		super(Label.OPEN, receiver);
+		addStringField("filename", Message.openFile());
 	}
 
 	@Override
 	protected final void execute() throws CommandException {
-
 		try {
-			// FIXME implement command
+			_receiver.load(stringField("filename"));
 		} catch (UnavailableFileException e) {
 			throw new FileOpenFailedException(e);
 		}
--- PO/src/xxl/app/main/DoSave.java
+++ PO/src/xxl/app/main/DoSave.java
@@ -1,21 +1,37 @@
 package xxl.app.main;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import pt.tecnico.uilib.forms.Form;
 import pt.tecnico.uilib.menus.Command;
+import pt.tecnico.uilib.menus.CommandException;
 import xxl.core.Calculator;
-// FIXME import classes
+import xxl.core.exception.MissingFileAssociationException;
+import xxl.core.exception.UnavailableFileException;
 
 /**
  * Save to file under current name (if unnamed, query for name).
  */
 class DoSave extends Command<Calculator> {
 
-  DoSave(Calculator receiver) {
-    super(Label.SAVE, receiver, xxl -> xxl.getSpreadsheet() != null);
-  }
-  
-  @Override
-  protected final void execute() {
-    // FIXME implement command and create a local Form
-  }
+	DoSave(Calculator receiver) {
+		super(Label.SAVE, receiver, xxl -> xxl.getSpreadsheet() != null);
+		addStringField("filename", Message.saveAs());
+	}
+
+	@Override
+	protected final void execute() throws CommandException {
+		try {
+			if (_receiver.getSpreadsheet().getFilename() == null) {
+				addStringField("filename", Message.saveAs());
+				_receiver.saveAs(stringField("filename"));
+			} else {
+				_receiver.save();
+			}
+		} catch (IOException | MissingFileAssociationException e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
 }
