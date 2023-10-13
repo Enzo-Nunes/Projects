8ff1ad808ea01995955a9107e3aa5341e219b48d
ArchKaiser <pedralhoenzo@gmail.com>
2023-10-13 20:03:10 +0100

    format start

--- PO/xxl/app/App.java
+++ PO/xxl/app/App.java
@@ -8,20 +8,20 @@ import xxl.core.exception.ImportFileException;
  */
 public class App {
 
-  public static void main(String[] args) {
-    try (var ui = Dialog.UI) {
-      var receiver = new xxl.core.Calculator();
-      String datafile = System.getProperty("import");
-      if (datafile != null) {
-        try {
-          receiver.importFile(datafile);
-        } catch (ImportFileException e) {
-          // no behavior described: just present the problem
-          e.printStackTrace();
-        }
-      }
-      
-      (new xxl.app.main.Menu(receiver)).open();
-    }
-  }
+	public static void main(String[] args) {
+		try (var ui = Dialog.UI) {
+			var receiver = new xxl.core.Calculator();
+			String datafile = System.getProperty("import");
+			if (datafile != null) {
+				try {
+					receiver.importFile(datafile);
+				} catch (ImportFileException e) {
+					// no behavior described: just present the problem
+					e.printStackTrace();
+				}
+			}
+
+			(new xxl.app.main.Menu(receiver)).open();
+		}
+	}
 }
