7f2ce1d7a3e52ed6bfa2b5584990b07177a9d3a3
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2023-10-14 03:01:52 +0100

    calorios project cleanup

--- FP/Caloiros2023/project1.py
+++ FP/Caloiros2023/project1.py
@@ -1,7 +1,9 @@
-alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-
-def eh_territorio(arg):
+def eh_territorio(arg) -> bool:
+	"""	Recebe um argumento de qualquer tipo e devolve True se o seu argumento
+		corresponde a um territorio e False caso contrario."""
+	
 	if not isinstance(arg, tuple):
 		return False
 	for x in arg:
@@ -14,114 +16,153 @@ def eh_territorio(arg):
 				return False
 			if y != 0 and y != 1:
 				return False
+			
 	return True
 
 
-def obtem_ultima_intersecao(t):
-	return 	alfabeto[len(t)-1], len(t[0])
+def obtem_ultima_intersecao(t:tuple) -> tuple:
+	""" Recebe um territorio e devolve a intersecao do extremo superior
+		direito do territorio."""
+	
+	if not eh_territorio(t):
+		raise ValueError('obtem_ultima_intersecao: argumento invalido')
+	
+	return ALFABETO[len(t)-1], len(t[0])
 
 
 def eh_intersecao(arg):
+	""" Recebe um argumento de qualquer tipo e devolve True se o seu argumento
+		corresponde a uma intersecao e False caso contrario."""
+	
 	if not isinstance(arg, tuple):
+		return False
+	
+	if len(arg) != 2:
 		return False
 	if not isinstance(arg[0], str):
 		return False
+	if arg[0] not in ALFABETO:
+		return False
+	
 	if not isinstance(arg[1], int):
 		return False
+	if arg[1] < 1 or arg[1] > 99:
+		return False
+
 	return True
 
 
-def eh_intersecao_valida(t, i):
+def eh_intersecao_valida(t:tuple, i:tuple) -> bool:
+	""" Recebe um territorio e uma intersecao e devolve True se o seu argumento
+		corresponde a uma intersecao valida nesse territorio e False caso contrario."""
+
+	last = obtem_ultima_intersecao(t)
+
 	if not eh_territorio(t):
 		return False
 	if not eh_intersecao(i):
 		return False
-	if 'A' <= i[0] <= alfabeto[len(t)-1] and 1 <= i[1] <= len(t[0]):
-		return True
-	return False
+	if not i[0] <= last[0]:
+		return False
+	if not i[1] <= last[1]:
+		return False
+	return True
 
 
-def eh_intersecao_livre(t, i):
+def eh_intersecao_livre(t:tuple, i:tuple) -> bool:
+	""" Recebe um territorio e uma intersecao e devolve True se o seu argumento
+		corresponde a uma intersecao sem montanha nesse territorio e False caso contrario."""
+
 	if not eh_intersecao_valida(t, i):
 		return False
-	if t[alfabeto.index(i[0])][i[1]-1] == 0:
-		return True
-	return False
+	return not t[ALFABETO.index(i[0])][i[1]-1]
 
 
-def obtem_intersecoes_adjacentes(t, i):
+def obtem_intersecoes_adjacentes(t:tuple, i:tuple) -> tuple:
+	""" Recebe um territorio e uma intersecao e devolve uma tuplo com as intersecoes
+		validas adjacentes da intersecao dada."""
 
-	# Limits
-	upper = len(t[0]) + 1
-	lower = 0
-	left = ""
-	right = alfabeto[len(t)]
+	# Limites do Territorio
+	lower = 1
+	left  = "A"
+	right = obtem_ultima_intersecao(t)[0]
+	upper = obtem_ultima_intersecao(t)[1]
 
-	# Adjacent Positions
-	col_esquerda	= alfabeto[alfabeto.index(i[0]) - 1] if i[0] != "A" else ""
-	col_direita		= alfabeto[alfabeto.index(i[0]) + 1] if i[0] != "Z" else ""
-	lin_baixo		= i[1] - 1
-	lin_cima		= i[1] + 1
-
-	# Adjacent Intersections
-	esquerda = (col_esquerda, i[1])	if col_esquerda != left	 else ()
-	direita	 = (col_direita, i[1])	if col_direita	!= right else ()
-	baixo	 = (i[0], lin_baixo)	if lin_baixo	!= lower else ()
-	cima	 = (i[0], lin_cima)		if lin_cima		!= upper else ()
+	# Posições adjacentes
+	esquerda = (ALFABETO[ALFABETO.index(i[0]) - 1], i[1]) if i[0] != left  else ()
+	direita	 = (ALFABETO[ALFABETO.index(i[0]) + 1], i[1]) if i[0] != right else ()
+	baixo	 = (ALFABETO[ALFABETO.index(i[0])], i[1] - 1) if i[1] != lower else ()
+	cima	 = (ALFABETO[ALFABETO.index(i[0])], i[1] + 1) if i[1] != upper else ()
 
 	return tuple(x for x in (baixo, esquerda, direita, cima) if x != ())
 
 
-def ordena_intersecoes(tup):
+def ordena_intersecoes(tup:tuple) -> tuple:
+	""" Recebe um tuplo de intersecoes e devolve um tuplo com as intersecoes
+		ordenadas de acordo com a ordem de leitura do territorio."""
 	return tuple(sorted(tup, key=lambda x: (x[1], x[0])))
 
 
-def territorio_para_str(t):
+def territorio_para_str(t:tuple) -> str:
+	""" Recebe um territorio e devolve uma cadeia de caracteres que o representa."""
 	if not eh_territorio(t):
 		raise ValueError('territorio_para_str: argumento invalido')
-	
+
+	# Dimensoes do territorio
 	cols = len(t)
 	rows = len(t[0])
 	field = "  "
 
+	# Primeira linha
 	for i in range(cols):
-		field += " " + alfabeto[i]
+		field += " " + ALFABETO[i]
 	field += "\n"
 
+	# Corpo
 	for i in range(rows, 0, -1):
 		field += " " if i < 10 else ""
 		field += str(i) + " "
 		for j in range(cols):
-			field += ". " if eh_intersecao_livre(t, (alfabeto[j], i)) else "X "
+			field += ". " if eh_intersecao_livre(t, (ALFABETO[j], i)) else "X "
 		field += " " if i < 10 else ""
 		field += str(i) + "\n"
 
+	# Ultima linha
 	field += "  "
 	for i in range(cols):
-		field += " " + alfabeto[i]
+		field += " " + ALFABETO[i]
 
 	return field
 
 
-def obtem_cadeia(t, i):
+def obtem_cadeia(t:tuple, i:tuple) -> tuple:
+	""" Recebe um territorio e uma intersecao e devolve um tuplo ordenado com
+		todas as intersecoes connectadas a i."""
+	
 	if not eh_intersecao_valida(t, i):
 		raise ValueError('obtem_cadeia: argumentos invalidos')
 
-	toCheck = [i]
-	checked = []
+	# Intersecoes a verificar
+	stack = [i]
+	# Intersecoes verificadas (resultado)
+	res = []
 
-	while len(toCheck) > 0:
-		current = toCheck.pop()
-		if current not in checked:
-			checked.append(current)
+	# Algoritmo de busca em profundidade.
+	while len(stack) > 0:
+		current = stack.pop()
+		if current not in res:
+			res.append(current)
 			for x in obtem_intersecoes_adjacentes(t, current):
-				if x not in checked and x not in toCheck and eh_intersecao_livre(t, x) == eh_intersecao_livre(t, i):
-					toCheck.append(x)
-	
-	return ordena_intersecoes(tuple(checked))
-		
+				if x not in res and x not in stack and eh_intersecao_livre(t, x) == eh_intersecao_livre(t, i):
+					stack.append(x)
 
-def obtem_vale(t, i):
+	return ordena_intersecoes(tuple(res))
+
+
+def obtem_vale(t:tuple, i:tuple) -> tuple:
+	""" Recebe um territorio e uma intersecao e devolve um tuplo ordenado com
+		todas as intersecoes livres adjacentes a cadeia de i."""
+	
 	if not eh_intersecao_valida(t, i):
 		raise ValueError('obtem_vale: argumentos invalidos')
 	if eh_intersecao_livre(t, i):
@@ -136,13 +177,18 @@ def obtem_vale(t, i):
 	return ordena_intersecoes(tuple(vale))
 
 
-def verifica_conexao(t, i1, i2):
+def verifica_conexao(t:tuple, i1:tuple, i2:tuple) -> bool:
+	""" Recebe um territorio e duas intersecoes e devolve True se as intersecoes
+		pertencem a uma mesma cadeia e False caso contrario."""
+	
 	if not eh_intersecao_valida(t, i1) or not eh_intersecao_valida(t, i2):
 		raise ValueError('verifica_conexao: argumentos invalidos')
-	return i2 in obtem_cadeia(t, i1) and eh_intersecao_livre(t, i1) == eh_intersecao_livre(t, i2)
+	return i2 in obtem_cadeia(t, i1)
 
 
-def calcula_numero_montanhas(t):
+def calcula_numero_montanhas(t:tuple) -> int:
+	""" Recebe um territorio e devolve o numero de montanhas do territorio."""
+
 	if not eh_territorio(t):
 		raise ValueError('calcula_numero_montanhas: argumento invalido')
 	res = 0
@@ -152,43 +198,51 @@ def calcula_numero_montanhas(t):
 	return res
 
 
-def getMontanhas(t):
+def obtem_montanhas(t:tuple) -> list:
+	""" Recebe um territorio e devolve uma lista com todas as suas intersecoes
+		que	contem montanhas."""
+	
 	res = []
 	for i in range(len(t)):
 		for j in range(len(t[i])):
-			if not eh_intersecao_livre(t, (alfabeto[i], j+1)):
-				res.append((alfabeto[i], j+1))
+			if not eh_intersecao_livre(t, (ALFABETO[i], j+1)):
+				res.append((ALFABETO[i], j+1))
 	return res
 
 
-def calcula_numero_cadeias_montanhas(t):
+def calcula_numero_cadeias_montanhas(t:tuple) -> int:
+	""" Recebe um territorio e devolve o numero de cadeias de montanhas do territorio."""
+
 	if not eh_territorio(t):
 		raise ValueError('calcula_numero_cadeias_montanhas: argumento invalido')
-	
-	res = 0
-	checked = []
-	toCheck = getMontanhas(t)
 
-	while len(toCheck) > 0:
-		current = toCheck.pop()
+	# Numero de Cadeias de montanhas
+	res = 0
+	# Intersecoes com montanhas
+	stack = obtem_montanhas(t)
+
+	while len(stack) > 0:
+		current = stack.pop()
 		for x in obtem_cadeia(t, current):
-			if x not in checked:
-				checked.append(x)
-			if x in toCheck:
-				toCheck.remove(x)
+			if x in stack:
+				stack.remove(x)
 		res += 1
+
 	return res
 
 
-def calcula_tamanho_vales(t):
+def calcula_tamanho_vales(t:tuple) -> int:
+	""" Recebe um territorio e devolve o numero de intersecoes livres que
+		pertencem a vales do territorio."""
+	
 	if not eh_territorio(t):
 		raise ValueError('calcula_tamanho_vales: argumento invalido')
-	
-	toCheck = getMontanhas(t)
-	vales = []
 
-	while len(toCheck) > 0:
-		current = toCheck.pop()
+	vales = []
+	stack = obtem_montanhas(t)
+
+	while len(stack) > 0:
+		current = stack.pop()
 		for x in obtem_vale(t, current):
 			if x not in vales:
 				vales.append(x)
