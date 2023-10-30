2b8a05ca419e8b7afdbaa05613efe829c8a62655
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2023-10-30 14:19:05 +0000

    small stuff related to User.java po teste pratico

--- PO/xxl/core/User.java
+++ PO/xxl/core/User.java
@@ -1,13 +1,28 @@
 package xxl.core;
 
-public class User {
-	private String _name;
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class User implements Serializable {
+	private final String _name;
+	private List<Spreadsheet> _spreadsheets = new ArrayList<>();
 
 	public User(String name) {
 		_name = name;
 	}
 
-	public String getName() {
+	public final String getName() {
 		return _name;
 	}
+
+	void add(Spreadsheet s) {
+		_spreadsheets.add(s);
+	}
+
+	public List<Spreadsheet> getSpreadSheets() {
+		return Collections.unmodifiableList(_spreadsheets);
+	}
+
 }
