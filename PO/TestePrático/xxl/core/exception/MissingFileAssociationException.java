b19eacd7e50ff05d63f0c5c14cf29d740f31cbb1
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2023-11-01 18:11:20 +0000

    teste pratico cu krl

--- PO/TestePrático/xxl/core/exception/MissingFileAssociationException.java
+++
@@ -1,7 +0,0 @@
-package xxl.core.exception;
-
-/**
- * Thrown when an application is not associated with a file.i
- */
-public class MissingFileAssociationException extends Exception {
-}
--- PO/TestePrático/xxl/core/exception/SpreadsheetSizeException.java
+++
@@ -1,7 +0,0 @@
-package xxl.core.exception;
-
-public class SpreadsheetSizeException extends Exception {
-	public SpreadsheetSizeException(String msg) {
-		super(msg);
-	}
-}
