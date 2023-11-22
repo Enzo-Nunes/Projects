99c8c784bc5e0f7740a5ab1961281bc89410232a
Enzo Nunes <pedralhoenzo@gmail.com>
2023-11-22 03:13:18 +0000

    trying more stuff

--- ASA/Project1/project1.cpp
+++ ASA/Project1/project1.cpp
@@ -1,37 +1,54 @@
 #include <iostream>
 #include <vector>
 
-#define SHAPE vector<int>(3, 0)
+#define max(a, b) (a > b ? a : b)
 using namespace std;
 
-void processInput(int &FieldWidth, int &FieldHeight, int &NumShapes, vector<vector<int>> &ShapeTypes) {
+typedef struct ShapeType {
+	int width, height, price;
+} ShapeType;
+
+void processInput(int &FieldWidth, int &FieldHeight, int &NumShapes, vector<ShapeType> &ShapeTypes) {
 	// Read the first two lines containing the number of lines, columns and shapes
-	char comma;
-	cin >> FieldWidth >> comma >> FieldHeight;
-	cin >> NumShapes;
+	cin >> FieldWidth >> FieldHeight >> NumShapes;
 
 	// Populate the shapes matrix
 	for (int i = 0; i < NumShapes; i++) {
 		int width, height, price;
 		cin >> width >> height >> price;
 
-		ShapeTypes.push_back(SHAPE);
-		ShapeTypes[i][0] = width;
-		ShapeTypes[i][1] = height;
-		ShapeTypes[i][2] = price;
+		ShapeType newShape;
+		newShape.width	= width;
+		newShape.height	= height;
+		newShape.price	= price;
+		ShapeTypes.push_back(newShape);
 	}
 }
 
-int getMaxPrice(vector<vector<vector<int>>> &FieldPermutations) {}
+void addRotations(int &NumShapes, vector<ShapeType> &ShapeTypes) {
+	// Add the rotated shapes to the shapes matrix
+	for (int i = 0; i < NumShapes; i++) {
+		ShapeType newShape;
+		newShape.width	= ShapeTypes[i].height;
+		newShape.height	= ShapeTypes[i].width;
+		newShape.price	= ShapeTypes[i].price;
+		ShapeTypes.push_back(newShape);
+	}
+	NumShapes *= 2;
+}
+
+// Dynamic programming solution
+int getMaxFieldPrice(int FieldWidth, int FieldHeight, int NumShapes, vector<ShapeType> &ShapeTypes) {}
 
 int main() {
-	int FieldWidth, FieldHeight, NumShapes, MaxPrice = 0;
-	vector<vector<int>> ShapeTypes;
+	int FieldWidth, FieldHeight, NumShapes;
+	vector<ShapeType> ShapeTypes;
 
 	// Process the input
 	processInput(FieldWidth, FieldHeight, NumShapes, ShapeTypes);
+	addRotations(NumShapes, ShapeTypes);
 
 	// Print the maximum price and finish
-	cout << getMaxPrice(FieldPermutations) << endl;
+	cout << getMaxFieldPrice(FieldWidth, FieldHeight, NumShapes, ShapeTypes) << endl;
 	return 0;
 }
