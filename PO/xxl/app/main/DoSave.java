edb5866f49a8811670708d05ea9c4303b87633a8
ist1106336 <enzo.nunes@tecnico.ulisboa.pt>
2024-03-02 19:15:16 +0000

    PO app formatting

---
+++ PO/xxl/app/main/DoSave.java
@@ -0,0 +1,34 @@
+package xxl.app.main;
+
+import java.io.IOException;
+
+import pt.tecnico.uilib.menus.Command;
+import pt.tecnico.uilib.menus.CommandException;
+import xxl.core.Calculator;
+import xxl.core.exception.MissingFileAssociationException;
+
+/**
+ * Save to file under current name (if unnamed, query for name).
+ */
+class DoSave extends Command<Calculator> {
+
+	DoSave(Calculator receiver) {
+		super(Label.SAVE, receiver, xxl -> xxl.getSpreadsheet() != null);
+		addStringField("filename", Message.newSaveAs());
+	}
+
+	@Override
+	protected final void execute() throws CommandException {
+		try {
+			if (_receiver.getSpreadsheet().getFilename() == null) {
+				_receiver.saveAs(stringField("filename"));
+			} else {
+				_receiver.save();
+			}
+			_receiver.getSpreadsheet().setDirty(false);
+		} catch (IOException | MissingFileAssociationException e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
+}
